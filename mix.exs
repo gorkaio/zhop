eedcfbb9a47ba93aa0fe3414fb0a5031ba8c95c8
Gorka López de Torre <glopezdetorre@gmail.com>
2018-04-14 18:13:17 +0200

    Add coverex support

--- mix.exs
+++ mix.exs
@@ -5,7 +5,10 @@ defmodule Zhop.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [
+        tool: Coverex.Task
+      ]
     ]
   end
 
@@ -15,6 +18,8 @@ defmodule Zhop.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:coverex, "~> 1.4.10", only: :test}
+    ]
   end
 end
